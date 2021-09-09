5a049df43ff1f4d20a2bb1d02732c4a0d0d499d8
Donovan Searles <donovansearles@gmail.com>
2021-09-09 11:28:29 -0400

    more notes

--- PROJECT - 02/warm_up_code/war-game.py
+++ PROJECT - 02/warm_up_code/war-game.py
@@ -38,3 +38,26 @@ class Deck:
     def deal_one(self):
         # Note we remove one card from the list of all_cards
         return self.all_cards.pop()
+
+class Player:
+    
+    def __init__(self,name):
+        self.name = name
+        # A new player has no cards
+        self.all_cards = [] 
+        
+    def remove_one(self):
+        # Note we remove one card from the list of all_cards
+        # We state 0 to remove from the "top" of the deck
+        # We'll imagine index -1 as the bottom of the deck
+        return self.all_cards.pop(0)
+    
+    def add_cards(self,new_cards):
+        if type(new_cards) == type([]):
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+    
+    
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
